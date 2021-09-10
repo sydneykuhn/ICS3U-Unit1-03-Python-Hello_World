fe3423c656b1d02ee58b3f598c329d0f449fc52b
Ubuntu <ubuntu@ip-172-31-90-143.ec2.internal>
2021-09-10 16:43:19 +0000

    added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Sept 2020
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
